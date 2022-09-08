03655a31c39e72222c0468a942688681ef9b7654
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2022-09-08 21:48:49 +1000

    chore: Release script
    Signed-off-by: ♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
    Signed-off-by: ♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+    echo "You must specify a tag"
+    exit
+fi
+
+git-cliff --tag $1 > CHANGELOG.md
+git add ./CHANGELOG.md
+git commit -m "chore(release): $1"
+git tag "$1"
+
+echo "Finished! Please push the changes"
